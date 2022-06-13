5c7681d02b39802015dd6413f09a6a66d8200f31
Gabriel Borges <gbdesigns13@gmail.com>
2022-06-12 21:34:55 -0300

    Add Data to your Database through Remix Actions

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,12 +1,25 @@
-import type { LoaderFunction } from "@remix-run/node";
+import type { ActionFunction, LoaderFunction} from "@remix-run/node";
+import { redirect } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import type { Post } from "~/services/posts.server";
+import { createPost } from "~/services/posts.server";
 import { Post as PostComponent } from '~/components/Post';
 import { getPosts  } from '~/services/posts.server';
+import { PostForm } from "~/components/PostForm";
 
 type LoaderData = {
   posts: Post[];
+}
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData();
+  const rawTitle = form.get("title");
+  const rawBody = form.get("body");
+
+  const post = await createPost({ title: rawTitle, body: rawBody });
+
+  return redirect("/")
 }
 
 export const loader: LoaderFunction = async () => {
@@ -18,8 +31,8 @@ export default function Index() {
   const { posts } = useLoaderData<LoaderData>();
 
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+    <div className="flex flex-col items-center">
+      <PostForm method="post" action="/?index" />
       <ul>
         {posts.map(post => (
           <li key={post.title}>
--- app/services/posts.server.ts
+++ app/services/posts.server.ts
@@ -1,4 +1,14 @@
+import type { Post } from "@prisma/client";
 import { db } from "~/services/db.server";
 export type { Post } from "@prisma/client";
 
 export const getPosts = () => db.post.findMany();
+
+export const createPost = ({ title, body }: Pick<Post, "title" | "body">) => {
+  return db.post.create({
+    data: {
+      title,
+      body,
+    },
+  });
+};
