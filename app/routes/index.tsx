be193adc4051eb8b70f7b585f44d7b8f2e671350
Gabriel Borges <gbdesigns13@gmail.com>
2022-06-12 21:05:18 -0300

    Retrieve and Display Data and Remix Loaders

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,17 +1,20 @@
 import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import type { Post  } from '~/services/posts.server';
+import { getPosts  } from '~/services/posts.server';
 
-export const loader: LoaderFunction = () => {
-  return [{"title": "first", "body": "My first post"}]
+type LoaderData = {
+  posts: Post[];
 }
 
-type Post = {
-  title: string;
-  body: string;
+export const loader: LoaderFunction = async () => {
+  const data: LoaderData = { posts: await getPosts()}
+  return json(data);
 }
 
 export default function Index() {
-  const posts = useLoaderData<Post[]>();
+  const { posts } = useLoaderData<LoaderData>();
 
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
