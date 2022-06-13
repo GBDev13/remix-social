161de0e0995225402ac66d6d27b28946593029f6
Gabriel Borges <gbdesigns13@gmail.com>
2022-06-12 23:21:15 -0300

    Validate User SignUp Data and Hash a Password

--- app/routes/__app/login.tsx
+++ app/routes/__app/login.tsx
@@ -1,10 +1,20 @@
+import { useTransition } from '@remix-run/react'
+import { Button } from '~/components/Button'
 import { UserForm } from '~/components/UserForm'
 
 export default function SignInPage() {
+  const transition = useTransition()
+
   return (
-    <div>
+    <div className="max-w-sm mx-auto">
       <h1 className="text-xl text-slate-800 mb-8">Sign in</h1>
-      <UserForm />
+      <UserForm>
+        <Button type="submit" disabled={transition.state !== 'idle'}>
+          {transition.state === 'submitting' || 'loading'
+            ? 'Login'
+            : 'Logging in....'}
+        </Button>
+      </UserForm>
     </div>
   )
 }
